44d96e4ce1109f988e504b7e1f41a77292cc9d22
coffeestone02 <ygt2120@naver.com>
2025-06-07 16:46:29 +0900

    개발자 확인용 에셋, 자동차(탈출 지점), 키보드 아이콘 추가

---
+++ Assets/Scripts/EnvironmentAudioPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnvironmentAudioPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource bgmPlayer;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
